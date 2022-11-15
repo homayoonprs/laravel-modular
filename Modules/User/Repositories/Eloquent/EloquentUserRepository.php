f590eebc3e58d7a4506cdb6c6f84d40b06ea982d
Homayoon Parsaee <homayoon.parsaee@gmail.com>
2022-11-15 18:41:51 +0330

    create user Repository

---
+++ Modules/User/Repositories/Eloquent/EloquentUserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\User\Repositories\Eloquent;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
+use Modules\User\Entities\User;
+
+class EloquentUserRepository extends EloquentBaseRepository
+{
+
+}
